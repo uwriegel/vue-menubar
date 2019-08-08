4c17790ee57c52907021ec56a9622a502b548613
uriegel <uriegel@hotmail.de>
2019-08-08 18:50:08 +0200

    Now correctly exporting global function

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import MainMenu from "./MainMenu.vue"
-import makeKey from './accelerators'
+import { makeKey } from './accelerators'
 
 export default {
     install(Vue, options) {
         // Let's register our component globally
         // https://vuejs.org/v2/guide/components-registration.html
         Vue.component("vue-menu-bar", MainMenu)
-        Vue.vueMenuMakeKey = makeKey
+        Vue.menuMakeKey = makeKey
     }
 }
