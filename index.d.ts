ef3f8daaff04a1df3cb1129de00c89abbc5b746a
uriegel <uriegel@hotmail.de>
2019-10-25 23:00:09 +0200

    interface menuitem

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,8 @@
 declare module 'vue-menubar' {
     import { PluginFunction } from "vue"
     export const install: PluginFunction<{}>
+
+    interface MenuItem {
+        isSelected: boolean
+    }
 }
